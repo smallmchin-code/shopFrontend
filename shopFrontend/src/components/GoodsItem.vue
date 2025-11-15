a57245b13eca7cf60dbf3e7d5c9b07e55f3847d1
Sean <smallmchin@gmail.com>
2025-11-15 17:17:01 +0800

    測試

---
+++ shopFrontend/src/components/GoodsItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="goods-item">
+    <img :src="image" :alt="name" />
+    <h3>{{ name }}</h3>
+    <p>${{ price }}</p>
+
+  </div>
+</template>
+
+<script setup>
+const props = defineProps ({
+  name: String,
+  image: String,
+  price: Number,
+  size: String,
+  quantity: Number,
+  type: String,
+})
+</script>
+
+<style scoped>
+.goods-item {
+  border: 0px solid #ccc;
+  padding: 16px;
+  text-align: center;
+}
+
+.goods-item img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
